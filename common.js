77a18264646e1395ec45233a80af47e289f21dc4
vivekb <vivek214325@gmail.com>
2024-11-14 19:42:29 +0530

    add new page

--- common.js
+++ common.js
@@ -75,6 +75,53 @@ $(document).ready(function() {
 
   }
   });
-  
+
+// on hover content
+document.addEventListener('DOMContentLoaded', function() {
+  const items = [
+      { selector: '.img1', title: 'Driver Do', description: 'Moving Vehicles on Autopilot' },
+      { selector: '.img2', title: 'iDream Movies', description: 'Media application for iDream Media, one of India\'s top media agencies' },
+      { selector: '.img3', title: 'Nistha Raj Official Website', description: 'Official website for the artist Nistha Raj' },
+      { selector: '.img4', title: 'Ram Charan Official App', description: 'An Official mobile app for Mega Power Star Ram Charan.' },
+      { selector: '.img5', title: 'Now Floats', description: 'Official Mobile App for Now Floats, designed for best performance' },
+      { selector: '.img6', title: 'Indian Movie Cop', description: 'IMC - Indian Movie Cop for piracy control, associated with the American embassy' },
+      { selector: '.img7', title: 'iDream Post', description: 'News Portal and mobile app for iDream Media' },
+      { selector: '.img8', title: 'GGEMT', description: 'Medical Mobile Application for US brand GGEMT' }
+  ];
+
+  items.forEach(item => {
+      const element = document.querySelector(item.selector);
+      if (element) {
+          element.addEventListener('mouseover', function() {
+              if (!element.querySelector('.hover-content')) {
+                  const hoverContent = document.createElement('div');
+                  hoverContent.classList.add('hover-content');
+                  hoverContent.innerHTML = `<h4 class="heading4">${item.title}</h4><h5 class="heading5">${item.description}</h5>`;
+                  element.appendChild(hoverContent);
+              }
+          });
+          element.addEventListener('mouseout', function() {
+              const hoverContent = element.querySelector('.hover-content');
+              if (hoverContent) element.removeChild(hoverContent);
+          });
+      }
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const loadMoreBtn = document.getElementById('load-more-btn');
+  const hiddenImages = document.querySelectorAll('.idea-all-images .img7, .idea-all-images .img8');
+
+  loadMoreBtn.addEventListener('click', function() {
+      hiddenImages.forEach(img => {
+          img.style.display = 'block';
+      });
+      // Disable the button to prevent further clicks
+      loadMoreBtn.disabled = true;
+  });
+});
+
+
   
 
